3a716dd3daa31848c8d68819c9a1178dfac0977d
max <maxbastienbalej@gmail.com>
2023-04-03 11:44:05 +0200

    Adding search by grep and by word

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,3 +5,5 @@ vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ")})
 end)
 
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
